807be89b1d450cf71ff99227327786e1b0b65ae6
bjartur20 <bjartur20@gmail.com>
2023-12-03 00:42:45 +0000

    code implemented

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
-	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import * as faceapi from 'face-api.js';
 
-	if (browser) {
+	onMount(() => {
 		const video = document.getElementById('webcam');
-		if (navigator.mediaDevices.getUserMedia) {
+
+		const setupWebcam = () => {
 			navigator.mediaDevices
 				.getUserMedia({ video: true })
 				.then(function (stream) {
@@ -12,9 +14,44 @@
 				.catch(function (error) {
 					console.log('Something went wrong!');
 				});
-		}
-	}
+		};
+
+		Promise.all([
+			faceapi.loadSsdMobilenetv1Model('/models'),
+			faceapi.loadFaceLandmarkModel('/models'),
+			faceapi.loadFaceRecognitionModel('/models')
+		]).then(setupWebcam);
+
+		video.addEventListener('play', async () => {
+			const canvas = faceapi.createCanvasFromMedia(video);
+			document.body.append(canvas);
+
+			const displaySize = { width: video.width, height: video.height };
+			faceapi.matchDimensions(canvas, displaySize, true);
+
+			setInterval(async () => {
+				const detections = await faceapi
+					.detectAllFaces(video)
+					.withFaceLandmarks()
+					.withFaceDescriptors();
+				console.log({ detections });
+
+				const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+				canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
+
+				detections.forEach((result, i) => {
+					const box = resizedDetections[i].detection.box;
+					const drawBox = new faceapi.draw.DrawBox(box, {
+						label: result
+					});
+					drawBox.draw(canvas);
+				});
+			}, 100);
+		});
+	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<video id="webcam" autoplay></video>
+<h1>Face Detect</h1>
+<!-- svelte-ignore a11y-media-has-caption -->
+<video id="webcam" width="600" height="450" style="width: 600px;height: 450px" autoplay></video>
